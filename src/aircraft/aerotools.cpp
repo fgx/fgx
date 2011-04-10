2f6c5e52140b721c7ad6bf0d97eabf6989518cbe
= <=>
2011-04-10 22:35:56 +0100

    OOps remove debug messages after merge

--- src/aircraft/aerotools.cpp
+++ src/aircraft/aerotools.cpp
@@ -26,21 +26,21 @@ AeroTools::AeroTools(QObject *parent) :
 /* This function walsk the /Aircraft/* directory
  its find files in a directory maching "*-set.xml"
    and therby engquires the xml fo
-   */
+*/
 
 QStringList AeroTools::scan_xml_sets(){
 
 	int c = 0;
 	int found = 0;
-	QProgressDialog progress("Recanning Aircraft to Cache", "Cancel", 0, 0);
+	QProgressDialog progress("Scanning Aircraft/ to Cache", "Cancel", 0, 0);
 	progress.setWindowModality(Qt::WindowModal);
 	progress.show();
 
 	QStringList aeroList;
 
 	QDir aircraftDir( settings.aircraft_path() );
-	qDebug() << settings.aircraft_path();
 	aircraftDir.setFilter( QDir::Dirs | QDir::NoSymLinks | QDir::NoDotAndDotDot);
+
 	QStringList entries = aircraftDir.entryList();
 	progress.setMaximum(entries.size());
 
@@ -48,13 +48,15 @@ QStringList AeroTools::scan_xml_sets(){
 
 		// Filter out default dir names, should be a QDir name filter?
 		if (*entry != "Instruments" &&  *entry != "Instruments-3d" && *entry != "Generic") {
+
 			progress.setValue(progress.value() + 1);
+
 			//** get the List of *-set.xml files in dir
 			QDir dir( settings.aircraft_path(*entry) );
 			QStringList filters;
 			filters << "*-set.xml";
 			QStringList list_xml = dir.entryList(filters);
-			qDebug() << list_xml;
+
 			if(list_xml.count() > 0){ // << Scan MOdels
 				QString directory;
 				QString description;
@@ -72,19 +74,22 @@ QStringList AeroTools::scan_xml_sets(){
 					aero = QString(xml_file);
 					aero.chop(8);
 
-					//** parse the Xml file - fucking long winded
+					//** parse the Xml file - f&*& long winded
 					QString file_path =  settings.aircraft_path(*entry);
 					file_path.append("/");
 					file_path.append(list_xml.at(i));
 					QFile xmlFile( file_path);
 					if (xmlFile.open(QIODevice::ReadOnly | QIODevice::Text)){
 
-						//* The filexontent are converted to UTF-8. Some files are Windows etc
+						/* The file content id converted to UTF-8.
+							 Some files are Windows, encoding and throw error with QxmlQuery etc
+							 Its a hack and dont quite understand whats happening.. said pedro
+						*/
 						QString xmlString = QString(xmlFile.readAll()).toUtf8();
 
 						QXmlQuery query;
 						query.setFocus(xmlString);
-						//query.setFocus(&xmlFile);
+						//query.setFocus(&xmlFile); << Because file is not QTF8 using sting instead
 						query.setQuery("PropertyList/sim");
 						if (query.isValid()){
 
@@ -107,13 +112,12 @@ QStringList AeroTools::scan_xml_sets(){
 					//#1 This version saves to settings and an embedded dam dirty delimiter string stuff..
 
 					QString record = QString("%1~|~%2~|~%3~|~%4~|~%5~|~%6").arg(directory, xml_file, aero, fdm, description, author );
-					qDebug() << record;
 					aeroList << record;
 
 					found++;
 
 					if(progress.wasCanceled()){
-						break;
+						break; // TODO ?? why..
 					}
 					c++;
 				} // <Add models
