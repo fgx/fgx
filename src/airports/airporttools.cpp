bcc15518b12624cfa89566becffe93088a8ddeb2
= <pete@daffodil.uk.com>
2011-04-23 01:08:15 +0100

    Aiport parser once and for f* all

--- src/airports/airporttools.cpp
+++ src/airports/airporttools.cpp
@@ -21,21 +21,51 @@ AirportTools::AirportTools(QObject *parent, MainObject *mOb) :
 
 
 //============================================================================
-//** Scan XML's for airports
+//== Scan XML's for airports
+// This function finds the threshold.xml files
+// The aiport CODE is derived from the file name in ICAO style
+// The runways are the nodes
+/*
+<?xml version="1.0"?>
+<PropertyList>
+  <runway>
+	<threshold>
+	  <lon>0.044298885776989</lon>
+	  <lat>51.505569223906</lat>
+	  <rwy>10</rwy>
+	  <hdg-deg>92.88</hdg-deg>
+	  <displ-m>95</displ-m>
+	  <stopw-m>55</stopw-m>
+	</threshold>
+	<threshold>
+	  <lon>0.065996952433288</lon>
+	  <lat>51.5048897753222</lat>
+	  <rwy>28</rwy>
+	  <hdg-deg>272.88</hdg-deg>
+	  <displ-m>71</displ-m>
+	  <stopw-m>90</stopw-m>
+	</threshold>
+  </runway>
+</PropertyList>
+*/
 //============================================================================
+
 void AirportTools::scan_airports_xml(){
+
 	int c = 0;
 	int found = 0;
+
+	//=================================
+	//** Show something as this takes time
 	QProgressDialog progress("Loading Airports to Cache", "Cancel", 0, 0);
 	progress.setWindowModality(Qt::WindowModal);
 	progress.setRange(0, 20000);
 	progress.show();
 
-	qDebug() << mainObject->settings->airports_path();
-	QString directory = mainObject->settings->airports_path();
 
-
-	//* Drop and recreate the Aiports table - then idex after import later	
+	//=================================
+	//* DataBase
+	//* Drop and recreate the Aiports table - then idex after import later
 	QStringList sql_commands;
 	sql_commands.append("DROP TABLE IF EXISTS airports;");
 	sql_commands.append("DROP TABLE IF EXISTS runways;");
@@ -52,34 +82,51 @@ void AirportTools::scan_airports_xml(){
 			return;
 		}
 	}
-	return;
-	//* Insert Aircraft query
-	QSqlQuery sqlApt(mainObject->db);
-	sqlApt.prepare("INSERT INTO airports(code)VALUES(?);");
+
+	//* Insert Airport query
+	QSqlQuery sqlAirportInsert(mainObject->db);
+	sqlAirportInsert.prepare("INSERT INTO airports(code)VALUES(?);");
+
+	//* Insert Runway Query
+	QSqlQuery sqlRunwayInsert(mainObject->db);
+	sqlRunwayInsert.prepare("INSERT INTO runways(airport_code, runway, heading, length, lat, lng)VALUES(?,?,?,?,?,?);");
 
 
-	//QStringList airportsList;PageName
+	//================================================
+	//* Lets Loop the directories
+	//qDebug() << mainObject->settings->airports_path();
+	QString airports_dir_path = mainObject->settings->airports_path();
 	QString entry;
-
-	QDirIterator it(directory, QDirIterator::Subdirectories);
+	QDirIterator it(airports_dir_path, QDirIterator::Subdirectories);
 	while (it.hasNext()) {
+
+		//* Update counter and progress periodically
 		c++;
 		if (c % 100 == 0){
 			progress.setValue(c);
 		}
+
+		//* Check if file entry is a *.threshold.xml
 		entry = it.next();
 		if(entry.endsWith(".threshold.xml") ){
-			QFileInfo info(entry);
-			//#airportsList << QString("%1~|~%2").arg(	entry,
-			//										info.fileName().split(".").at(0)
-			//
-			// Split out "FOO.threshold.xml"
-			QString code = info.fileName().split(".").at(0);
-			//qDebug() << entry << info.fileName() << code;
+
+			//** This is an airport - so import
+			QFileInfo fileInfoThreshold(entry);
+
+			//* Split out "<CODE>.threshold.xml" with a period
+			QString code = fileInfoThreshold.fileName().split(".").at(0);
+
+			//* Insert code to aiports table == primary key
 			sqlApt.bindValue(0, code);
 			if(!sqlApt.exec()){
-				qDebug() << mainObject->db.lastError();
+				qDebug() << "CRASH" << mainObject->db.lastError();
+				// TODO catch error log
+				return;
 			}
+
+			//* Parse the XMl file for runways
+
+
 			found++;
 		}
 
