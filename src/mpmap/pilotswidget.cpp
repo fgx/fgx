52beea1c4eef5865289f095b8c98934f1807c5a0
= <pete@daffodil.uk.com>
2011-07-03 00:14:17 +0100

    Pilots fetches from server

--- src/mpmap/pilotswidget.cpp
+++ src/mpmap/pilotswidget.cpp
@@ -1,7 +1,21 @@
 
+#include <QtDebug>
+
+
+#include <QtXml/QDomDocument>
+#include <QtXml/QDomNodeList>
+#include <QtXml/QDomNamedNodeMap>
+
+
+#include <QtNetwork/QNetworkRequest>
+#include <QtNetwork/QNetworkReply>
+
+
 #include <QVBoxLayout>
 #include <QToolBar>
 #include <QVBoxLayout>
+#include <QTreeWidgetItem>
+
 
 #include "pilotswidget.h"
 
@@ -10,6 +24,8 @@ PilotsWidget::PilotsWidget(MainObject *mob, QWidget *parent) :
 {
 
 	mainObject = mob;
+
+	netMan = new QNetworkAccessManager(this);
 
 	setMinimumWidth(300);
 
@@ -29,9 +45,13 @@ PilotsWidget::PilotsWidget(MainObject *mob, QWidget *parent) :
 	actionRefresh->setText("Refresh");
 	actionRefresh->setIcon(QIcon(":/icon/refresh"));
 	toolbar->addAction(actionRefresh);
+	connect(actionRefresh, SIGNAL(triggered()), this, SLOT(fetch_pilots()));
 
 	tree = new QTreeWidget();
 	mainLayout->addWidget(tree);
+
+	tree->setRootIsDecorated(false);
+	tree->setUniformRowHeights(true);
 
 	tree->headerItem()->setText(C_CALLSIGN, "Callsign");
 	tree->headerItem()->setText(C_AERO, "Aircraft");
@@ -44,3 +64,72 @@ PilotsWidget::PilotsWidget(MainObject *mob, QWidget *parent) :
 	statusBar->showMessage("Click refresh to load");
 
 }
+
+void PilotsWidget::fetch_pilots()
+{
+	server_string = "";
+	QUrl url("http://mpmap01.flightgear.org/fg_server_xml.cgi?mpserver01.flightgear.org:5001");
+	QNetworkRequest request;
+	request.setUrl(url );
+	qDebug() << url.toString();
+
+	reply = netMan->get(request);
+	connect(reply, SIGNAL( error(QNetworkReply::NetworkError)),
+			this, SLOT(on_server_error(QNetworkReply::NetworkError))
+	);
+	connect(reply, SIGNAL( readyRead()),
+			this, SLOT(on_server_ready_read())
+	);
+	connect(reply, SIGNAL( finished()),
+			this, SLOT(on_server_read_finished())
+	);
+
+}
+
+
+//==========================================================
+//= Server Events
+//==========================================================
+void PilotsWidget::on_server_error(QNetworkReply::NetworkError error){
+	qDebug() << "error" << error;
+}
+
+void PilotsWidget::on_server_ready_read(){
+	QString s(reply->readAll());
+	server_string.append(s);
+}
+
+void PilotsWidget::on_server_read_finished(){
+	qDebug() << "done"; // << server_string;
+
+	//= Create Dom Document
+	QDomDocument dom;
+	dom.setContent(server_string);
+
+	//= get the <fg_server> node
+	QDomNodeList nodes = dom.elementsByTagName("marker");
+	QStringList list;
+	qDebug() << nodes.length();
+	if (nodes.count() > 0){
+		for(int idxd =0; idxd < nodes.count(); idxd++){
+
+			QTreeWidgetItem *item = new QTreeWidgetItem();
+			tree->addTopLevelItem(item);
+
+			QDomNode node = nodes.at(idxd);
+			list << node.firstChildElement("marker").text();
+			QDomNamedNodeMap attribs =  node.attributes();
+			item->setText(C_CALLSIGN, attribs.namedItem("callsign").nodeValue());
+			item->setText(C_AERO, attribs.namedItem("model").nodeValue());
+			item->setText(C_ALTITUDE, attribs.namedItem("alt").nodeValue());
+		}
+	}
+
+}
+
+
+
+
+
+
+
