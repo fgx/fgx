8b7def18e38d76cd4ed2315054e31a3782dbd9ce
Raoul Quittarco <gral@who.net>
2011-07-03 14:51:19 +0200

    hide mpmap widget (experimental) for the moment

--- src/mpmap/mpmapwidget.cpp
+++ src/mpmap/mpmapwidget.cpp
@@ -50,8 +50,6 @@ MpMapWidget::MpMapWidget(MainObject *mOb, QWidget *parent) :
 	//**add callsign to url
 	QString mapURL1("http://mpmap01.flightgear.org/?follow=");
 	QString mapURL2("http://mpmap02.flightgear.org/?follow=");
-	mapURL1.append(mpmapFollowCallsign);
-	mapURL2.append(mpmapFollowCallsign);
     comboServer->addItem("MpMap-01", QVariant(mapURL1));
     comboServer->addItem("MpMap-02", QVariant(mapURL2));
     connect(comboServer, SIGNAL(currentIndexChanged(int)), this, SLOT(on_combo_server(int)));
@@ -99,13 +97,19 @@ void MpMapWidget::update_progress(int v){
 void MpMapWidget::end_progress(bool Ok){
 	Q_UNUSED(Ok);
     progressBar->setVisible(false);
-    statusBar->showMessage( comboServer->itemData(comboServer->currentIndex()).toString() );
+	QString mpmapFollowCallsign;
+	mpmapFollowCallsign.append(mainObject->actionCallsign->text());
+	QString statusURL = comboServer->itemData(comboServer->currentIndex()).toString();
+	statusURL.append(mpmapFollowCallsign);
+    statusBar->showMessage( statusURL );
 }
 
 
 void MpMapWidget::on_combo_server(int index){
 
     QString server_url = comboServer->itemData(index).toString();
+	//Add callsign to follow
+	server_url.append(mpmapFollowCallsign);
 	webView->setUrl(  QUrl(server_url)  );
     statusBar->showMessage(QString("Loading: ").append(server_url));
     //qDebug("on_combo");
