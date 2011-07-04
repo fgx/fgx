d13b821e41b8390ff28cac119121410c061d623b
Raoul Quittarco <gral@who.net>
2011-07-04 22:50:29 +0200

    fix for osx to show the gmap (no version 3.3 api call), to be reviewed

--- src/mpmap/xwebpage.h
+++ src/mpmap/xwebpage.h
@@ -4,11 +4,20 @@
 #include <QWebPage>
 
 class XWebPage : public QWebPage
+
 {
+	virtual QString userAgentForUrl(const QUrl& url) const {
+		return "Chrome/1.0";
+	};
+
+
 Q_OBJECT
 public:
-    explicit XWebPage(QObject *parent = 0);
+	
 
+	
+    explicit XWebPage(QObject *parent = 0);
+	
 	void javaScriptConsoleMessage ( const QString & message, int lineNumber, const QString & sourceID );
 signals:
 
