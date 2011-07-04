11c3c72e136fcfb7b3b73e0d2ee354a6b1d33b59
Raoul Quittarco <gral@who.net>
2011-07-04 22:55:34 +0200

    clean up header

--- src/mpmap/xwebpage.h
+++ src/mpmap/xwebpage.h
@@ -3,22 +3,15 @@
 
 #include <QWebPage>
 
-class XWebPage : public QWebPage
-
-{
-	virtual QString userAgentForUrl(const QUrl& url) const {
-		return "Chrome/1.0";
-	};
-
+class XWebPage : public QWebPage {
 
 Q_OBJECT
 public:
 	
-
-	
     explicit XWebPage(QObject *parent = 0);
 	
 	void javaScriptConsoleMessage ( const QString & message, int lineNumber, const QString & sourceID );
+
 signals:
 
 public slots:
