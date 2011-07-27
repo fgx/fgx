6978f84091f6472e6b89bca7b5974223eb54f5a2
Raoul Quittarco <gral@who.net>
2011-07-27 11:37:48 +0200

    metar: patch from geoff

--- src/airports/metarwidget.cpp
+++ src/airports/metarwidget.cpp
@@ -4,6 +4,7 @@
 
 
 #include "airports/metarwidget.h"
+#include "utilities/utilities.h"
 
 MetarWidget::MetarWidget(MainObject *mob, QWidget *parent) :
     QWidget(parent)
@@ -31,18 +32,53 @@ void MetarWidget::load_metar(QString apt)
 {
 	QProcess process;
 	QStringList args;
+    QString results;
+    QString metarPath;
+    QString tmp;
+    QString error_result;
+    QDir d;
+    metarPath = "metar";
 	args << "-r" << "-v" << apt;
+    if ( ! mainObject->X->fgroot_use_default() ) {
+        int ind, siz;
+        tmp = mainObject->X->fgfs_path();
+        ind = tmp.indexOf(QChar('/'));
+        siz = 0;
+        // march to last '/' in path
+        while (ind >= 0) {
+            tmp = tmp.mid(ind + 1);
+            siz = tmp.size();
+            ind = tmp.indexOf(QChar('/'));
+        }
+        if (siz > 0) {
+            tmp = mainObject->X->fgfs_path();
+            tmp.chop(siz);
+            if (d.exists(tmp)) {
+                metarPath = tmp + metarPath;
+            }
+        }
+    }
 
 	txtMetar->setPlainText( QString("Loading..\n\nmetar %1").arg(apt) );
-	process.start("metar", args, QIODevice::ReadOnly);
+    outLog("Running: ["+metarPath+" "+args.join(" ")+"]");
+    process.start(metarPath, args, QIODevice::ReadOnly);
 	if(process.waitForStarted()){
 		process.waitForFinished(3000);
-		QString ok_result = process.readAllStandardOutput();
-		QString error_result = process.readAllStandardError();
-		Q_UNUSED(error_result);
-		//qDebug() <<  error_result;
-		txtMetar->setPlainText(ok_result);
-	}
+        results = process.readAllStandardOutput();
+        error_result = process.readAllStandardError();
+        tmp = results.simplified();
+        if (tmp.size() == 0) {
+            if (error_result.size() > 0) {
+                results = error_result;
+            } else {
+                results = "NO metar results found!";
+            }
+        }
+        txtMetar->setPlainText(results);
+    } else {
+        results = "ERROR: Running of metar FAILED";
+    }
+    outLog("Results: "+results);
 }
 
 void MetarWidget::clear_metar()
