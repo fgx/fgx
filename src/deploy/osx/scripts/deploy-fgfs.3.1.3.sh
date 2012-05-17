1977bf54e9f6634280ced8be16cbe8c383de2bee
Raoul Quittarco <gral@who.net>
2012-05-17 11:06:39 +0200

    adding files

---
+++ src/deploy/osx/scripts/deploy-fgfs.3.1.3.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+FILES = "fgfs fgjs fgviewer jsdemo metar terrasync yasim yasim-proptest"
+
+for f in *
+
+do
+	# link OSG
+	install_name_tool -change libosgDB.92.dylib @executable_path/../Frameworks/libosgDB.92.dylib $f
+	install_name_tool -change libosgSim.92.dylib @executable_path/../Frameworks/libosgSim.92.dylib $f
+	install_name_tool -change libosgUtil.92.dylib @executable_path/../Frameworks/libosgUtil.92.dylib $f
+	install_name_tool -change libosg.92.dylib @executable_path/../Frameworks/libosg.92.dylib $f
+	install_name_tool -change libOpenThreads.12.dylib @executable_path/../Frameworks/libOpenThreads.12.dylib $f
+	install_name_tool -change libosgText.92.dylib @executable_path/../Frameworks/libosgText.92.dylib $f
+	install_name_tool -change libosgFX.92.dylib @executable_path/../Frameworks/libosgFX.92.dylib $f
+	install_name_tool -change libosgParticle.92.dylib @executable_path/../Frameworks/libosgParticle.92.dylib $f
+	install_name_tool -change libosgGA.92.dylib @executable_path/../Frameworks/libosgGA.92.dylib $f
+	install_name_tool -change libosgQt.92.dylib @executable_path/../Frameworks/libosgQt.92.dylib $f
+	install_name_tool -change libosgViewer.92.dylib @executable_path/../Frameworks/libosgViewer.92.dylib $f
+	install_name_tool -change libosgSim.92.dylib @executable_path/../Frameworks/libosgSim.92.dylib $f
+	install_name_tool -change libosgManipulator.92.dylib @executable_path/../Frameworks/libosgManipulator.92.dylib $f
+	install_name_tool -change libosgPresentation.92.dylib @executable_path/../Frameworks/libosgPresentation.92.dylib $f
+	install_name_tool -change libosgQt.92.dylib @executable_path/../Frameworks/libosgQt.92.dylib $f
+	install_name_tool -change libosgShadow.92.dylib @executable_path/../Frameworks/libosgShadow.92.dylib $f
+	install_name_tool -change libosgTerrain.92.dylib @executable_path/../Frameworks/libosgTerrain.92.dylib $f
+	install_name_tool -change libosgVolume.92.dylib @executable_path/../Frameworks/libosgVolume.92.dylib $f
+	install_name_tool -change libosgWidget.92.dylib @executable_path/../Frameworks/libosgWidget.92.dylib $f
+	
+	# link ALUT
+	install_name_tool -change /Users/jmt/Library/Frameworks/ALUT.framework/Versions/A/ALUT @executable_path/../Frameworks//ALUT.framework/Versions/A/ALUT $f
+	
+	# link libjepeg (imageio/cocoa related)
+	install_name_tool -change /opt/local/lib/libjpeg.8.dylib @executable_path/../Frameworks/libjpeg.8.dylib $f
+	
+	# Show result in log
+	otool -L $f
+done
---
+++ src/deploy/osx/scripts/deploy-osg-libs.3.1.3.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+install_name_tool -id libOpenThreads.12.dylib libOpenThreads.12.dylib
+install_name_tool -id libosg.92.dylib libosg.92.dylib
+install_name_tool -id libosgAnimation.92.dylib libosgAnimation.92.dylib
+install_name_tool -id libosgDB.92.dylib libosgDB.92.dylib
+install_name_tool -id libosgFX.92.dylib libosgFX.92.dylib
+install_name_tool -id libosgGA.92.dylib libosgGA.92.dylib
+install_name_tool -id libosgManipulator.92.dylib libosgManipulator.92.dylib
+install_name_tool -id libosgParticle.92.dylib libosgParticle.92.dylib
+install_name_tool -id libosgPresentation.92.dylib libosgPresentation.92.dylib
+install_name_tool -id libosgQt.92.dylib libosgQt.92.dylib
+install_name_tool -id libosgShadow.92.dylib libosgShadow.92.dylib
+install_name_tool -id libosgSim.92.dylib libosgSim.92.dylib
+install_name_tool -id libosgTerrain.92.dylib libosgTerrain.92.dylib
+install_name_tool -id libosgText.92.dylib libosgText.92.dylib
+install_name_tool -id libosgUtil.92.dylib libosgUtil.92.dylib
+install_name_tool -id libosgViewer.92.dylib libosgViewer.92.dylib
+install_name_tool -id libosgVolume.92.dylib libosgVolume.92.dylib
+install_name_tool -id libosgWidget.92.dylib libosgWidget.92.dylib
