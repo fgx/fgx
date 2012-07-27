a1a976316b1391b31826f816525b57805e450120
Yves Sablonier <gral@who.net>
2012-07-27 16:36:56 +0200

    Shifting version to 2.8.0rc3

--- src/main.cpp
+++ src/main.cpp
@@ -63,7 +63,7 @@ int main( int argc, char* argv[])
 	QApplication::setOrganizationName("FGx");
 	QApplication::setOrganizationDomain("fgx.ch");
 	QApplication::setApplicationName("FGxFlightGearLauncher");
-	QApplication::setApplicationVersion("2.8.0rc2");
+	QApplication::setApplicationVersion("2.8.0rc3");
 	
 	//=====================================================
 	//== Add fonts to database and set default application font
